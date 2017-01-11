0f7e1fabbc4d38f5f828dcc4c3f161a419b5eb43
Jarek Wojciechowski <jarekwojo@gmail.com>
2017-01-11 11:35:17 -0500

    git jumping to sections working, but still need to smooth out

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -2,9 +2,6 @@ document.addEventListener("DOMContentLoaded", function(event) {
   var about = document.getElementById('about-link');
   var portfolio = document.getElementById('portfolio-link');
   var contact = document.getElementById('contact-link');
-  about.onclick = smoothJump('#about');
-  portfolio.onclick = smoothJump('#portfolio');
-  contact.onclick = smoothJump('#contact');
 
 
   function smoothJump(idToJump){
@@ -12,4 +9,12 @@ document.addEventListener("DOMContentLoaded", function(event) {
       behavior: 'smooth' 
     });
   };
+
+  about.onclick = function() { smoothJump('#about'); };
+  portfolio.onclick = function(){ smoothJump('#portfolio'); };
+  contact.onclick = function(){ smoothJump('#contact'); };
+  document.getElementById('about-link').addEventListener('click', function(){smoothJump('#about')}, false);
+  document.getElementById('portfolio-link').addEventListener('click', function(){smoothJump('#portfolio')}, false);
+  document.getElementById('contact-link').addEventListener('click', function(){smoothJump('#contact')}, false);
 });
+
